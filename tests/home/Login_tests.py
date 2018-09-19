3747e913896fa73ebb8a7a087227d0fa55426f3d
shahanrahman <shahan.rahman1211@gmail.com>
2018-09-18 22:11:54 -0400

    Update Login_tests.py

--- tests/home/Login_tests.py
+++ tests/home/Login_tests.py
@@ -1,14 +1,9 @@
-import selenium
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 import unittest
 import pytest
 import os
 from pages.home.login_page import LoginPage
-from pages.home.member_page import MembersPage
 import time
-import utilities.logger as cl
-import logging
 
 
 
@@ -24,20 +19,24 @@ class LoginTests(unittest.TestCase):
 
     lp = LoginPage(driver)
 
-    @pytest.mark.run(order=2)
+    @pytest.mark.run(order=1)
     def test_validLogin(self):
 
         self.lp.login("shahan.rahman1211@gmail.com", "Gettrial123")
         time.sleep(5)
 
 
-    @pytest.mark.run(order=1)
+    @pytest.mark.run(order=2)
     def test_invalidLogin(self):
         self.driver.get(self.BaseURL)
         self.lp.login("shaef@gmail.com", "wrongPass")
         result = self.lp.verifyInvalidLogin()
-        time.sleep(5)
         assert result == True
+
+
+
+
+
 
 
 
