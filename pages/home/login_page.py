ed4d6bb8afc31689880560eae3256b7d43b6a946
shahanrahman <shahan.rahman1211@gmail.com>
2018-09-18 22:12:36 -0400

    Update login_page.py

--- pages/home/login_page.py
+++ pages/home/login_page.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from base.selenium_driver import SeleniumDriver
 import utilities.logger as cl
 import logging
+import time
 
 
 class LoginPage(SeleniumDriver):
@@ -18,6 +19,8 @@ class LoginPage(SeleniumDriver):
     _password = "//div[@class='input-row']//input[@id='password']"
     _login_button = "//div[@class='button-row']//button"
     _invalidLoginMsg = "//div[@class='error-message']"
+    _myAccount = "//div[@id='header__user__account']/a[@id='header__user__account__settings']"
+    _logOut = "//div[@class='account-logout']//button"
 
 
     ''' Basic actions'''
@@ -30,6 +33,12 @@ class LoginPage(SeleniumDriver):
     def clickLoginLink(self):
         self.elementClick(self._login_button, locatorType="xpath")
 
+    def myAccount(self):
+        self.elementClick(self._myAccount, locatorType="xpath")
+
+    def logOut(self):
+        self.elementClick(self._logOut, locatorType="xpath")
+
 
 
     ''' Functionality of Login Page '''
@@ -37,10 +46,16 @@ class LoginPage(SeleniumDriver):
         self.enterEmail(email)
         self.enterPassword(password)
         self.clickLoginLink()
+        time.sleep(5)
+        self.myAccount()
+        self.logOut()
+        time.sleep(4)
 
 
     def verifyInvalidLogin(self):
         result = self.isElementPresent(self._invalidLoginMsg, locatorType="xpath")
+
+
 
         return result
 
